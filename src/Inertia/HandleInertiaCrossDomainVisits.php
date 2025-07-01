385643df36216100f8c6379a92dd90cb28e73631
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2025-07-01 18:58:02 -0300

    Refactor Inertia cross-domain middleware for clarity and extendability
    - Extracted `previousHost()` method to simplify hostname comparison logic.
    - Added reference to inspiration and linked implementation details.

--- src/Inertia/HandleInertiaCrossDomainVisits.php
+++ src/Inertia/HandleInertiaCrossDomainVisits.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Inspired on Robert Boes implementation
+ *
+ * @link https://robertbo.es/2024/navigating-between-sub-domains-with-inertia
+ */
 class HandleInertiaCrossDomainVisits
 {
     /**
@@ -19,9 +24,13 @@ class HandleInertiaCrossDomainVisits
         return match (true) {
             ! $request->headers->has('X-Inertia'),
             ! $request->isMethod($request::METHOD_GET),
-            $request->host() === parse_url(url()->previous(), PHP_URL_HOST) => $next($request),
+            $request->host() === $this->previousHost() => $next($request),
             default => Inertia::location($request->fullUrl())
         };
+    }
 
+    protected function previousHost(): string
+    {
+        return parse_url(url()->previous(), PHP_URL_HOST);
     }
 }
