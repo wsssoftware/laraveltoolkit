07cb2829342fa50665f337d9b560c3cf0408489c
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2026-04-14 12:45:39 -0300

    Refactor `HasStoredAssets`: add fallback for `whenBooted` method

--- src/StoredAssets/HasStoredAssets.php
+++ src/StoredAssets/HasStoredAssets.php
@@ -14,10 +14,11 @@ trait HasStoredAssets
 
     protected static function bootHasStoredAssets(): void
     {
-
-        self::whenBooted(function () {
+        if (method_exists(static::class, 'whenBooted')) {
+            self::whenBooted(fn () => self::initStoredAssetFieldsAndRelations());
+        } else {
             self::initStoredAssetFieldsAndRelations();
-        });
+        }
         self::saving(function (Model $model) {
             foreach ($model->getDirty() as $field => $item) {
                 if (! $item instanceof Recipe) {
